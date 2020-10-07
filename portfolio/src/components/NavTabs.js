b16410ecf8a2a71270a0a82e5cdcc5f26593ccc5
MrBusiness87 <bbartula87@gmail.com>
2020-10-06 23:10:38 -0400

    10/06/2020-DEPLOYMENT

---
+++ portfolio/src/components/NavTabs.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export const PAGE_NAMES = {
+  HOME: "Home",
+  ABOUT: "About",
+  PORTFOLIO: "Portfolio",
+  CONTACT: "Contact",
+  NODE: "Node"
+}
+
+const navbar = {
+  textAlign:"center",
+}
+
+const navthumb = {
+  margin:"4px",
+  width:"50px",
+}
+
+function NavTabs(props) {
+  return (
+    <p class="navbar" style={navbar}>
+      <div class="links">
+        <a href="#home" onClick={() => props.handlePageChange("Home")} title="HOME" className="nav-link">
+        <img src={require ("./assets/links/home.png")} style={navthumb} />
+        </a>
+        <a href="#about" onClick={() => props.handlePageChange("About")} title="ABOUT" className="nav-link">
+        <img src={require ("./assets/links/about.png")} style={navthumb} />
+        </a>
+        <a href="#portfolio" onClick={() => props.handlePageChange("Portfolio")} title="PORTFOLIO" className="nav-link">
+        <img src={require ("./assets/links/portfolio.png")} style={navthumb} />
+        </a>
+        <a href="#node" onClick={() => props.handlePageChange("Node")} title="NODE" className="nav-link">
+        <img src={require ("./assets/links/nodejs.png")} style={navthumb} />
+        </a>
+      </div>
+    </p>
+  );
+}
+
+export default NavTabs;
---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NavTabs, { PAGE_NAMES } from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Node from "./pages/Node";
+
+class PortfolioContainer extends Component {
+  state = {
+    currentPage: PAGE_NAMES.HOME
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+  
+  renderTabs = () => {
+    switch (this.state.currentPage) {
+      case PAGE_NAMES.HOME:
+        return <Home />;
+      case PAGE_NAMES.ABOUT:
+        return <About />;
+      case PAGE_NAMES.PORTFOLIO:
+        return <Portfolio />;
+        case PAGE_NAMES.NODE:
+          return <Node />;
+      default:
+        return <Home />
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderTabs()}
+      </div>
+    );
+  }
+}
+
+export default PortfolioContainer;
