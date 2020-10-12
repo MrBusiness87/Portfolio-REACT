54f39be597eeab78dc11f538076543760a1171f7
MrBusiness87 <bbartula87@gmail.com>
2020-10-12 11:17:24 -0400

    10/12/2020-Final Push before deployment.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -26,7 +26,7 @@ function Portfolio() {
         <img src={require ("../assets/thumbnails/WEATHER thumbnail.png")} alt="WEATHER" />
       </a>
       <p class="ball"><br /><br /><br />
-      <h2 class="pTitle">PORTFOLIO<br />PAGE</h2>
+      <h2 class="title">PORTFOLIO<br />PAGE</h2>
       </p>
       <br />
       <a href="https://mrbusiness87.github.io/Work-Day-Scheduler/" target="_blank" style={floatL}>
@@ -36,13 +36,13 @@ function Portfolio() {
         <img src={require ("../assets/thumbnails/Scratch thumbnail.jpg")} alt="SCRATCH" />
       </a>
       <br />
-
       <a href="https://mrbusiness87.github.io/Finder-Project1/" target="_blank">
         <img src={require ("../assets/thumbnails/SF-thumbnail.jpg")} alt="SPORTS APP" />
       </a>
       <a href="https://your-kitchen.herokuapp.com/" target="_blank">
         <img src={require ("../assets/thumbnails/Project2.png")} alt="YOUR KITCHEN APP" />
       </a>
+      <br /><br /><br />
     </div>
   );
 }
