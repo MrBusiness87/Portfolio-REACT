2de0e7128841e4351c9a142f47b8b18c1a2a7479
MrBusiness87 <bbartula87@gmail.com>
2020-10-14 10:29:07 -0400

    Updates

---
+++ precache-manifest.61e842c65592d7424f4e81c59b3bf9d8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ce9c9088ff88f52b9e68b812283089",
+    "url": "/Portfolio-REACT/index.html"
+  },
+  {
+    "revision": "74741587a532435d1acb",
+    "url": "/Portfolio-REACT/static/js/2.66421c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-REACT/static/js/2.66421c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70597092debf8ab6120b",
+    "url": "/Portfolio-REACT/static/js/main.ce8b8bc9.chunk.js"
+  },
+  {
+    "revision": "df895739ca4c22547b11",
+    "url": "/Portfolio-REACT/static/js/runtime-main.7d34dd7d.js"
+  },
+  {
+    "revision": "2fd98bd954c61c08c68bd01e9b552cb8",
+    "url": "/Portfolio-REACT/static/media/Google thumbnail.2fd98bd9.jpg"
+  },
+  {
+    "revision": "b19f54ddefa41d5fb81e310bd4a3f038",
+    "url": "/Portfolio-REACT/static/media/LINKEDIN thumbnail.b19f54dd.jpg"
+  },
+  {
+    "revision": "672795767ab1b397ce63c3483e219413",
+    "url": "/Portfolio-REACT/static/media/NOTE thumbnail.67279576.png"
+  },
+  {
+    "revision": "b57aa82f40641d4765479ddd1e729465",
+    "url": "/Portfolio-REACT/static/media/Project2.b57aa82f.png"
+  },
+  {
+    "revision": "185a7b9bfa774c18dc7758b5d66b5c25",
+    "url": "/Portfolio-REACT/static/media/QUIZ thumbnail.185a7b9b.png"
+  },
+  {
+    "revision": "4cdcacb23b7313f0ae916d4234313451",
+    "url": "/Portfolio-REACT/static/media/READMEGEN thumbnail.4cdcacb2.png"
+  },
+  {
+    "revision": "fcd962993dab29921f5749a0cac15e1e",
+    "url": "/Portfolio-REACT/static/media/SF-thumbnail.fcd96299.jpg"
+  },
+  {
+    "revision": "7a08751a2c388e8bfb0819dcda10eb89",
+    "url": "/Portfolio-REACT/static/media/SQL_Employee_Tracker_GIF.7a08751a.gif"
+  },
+  {
+    "revision": "54b20d26ffb4b2c1d48f24ec8ba4aa1d",
+    "url": "/Portfolio-REACT/static/media/Scratch thumbnail.54b20d26.jpg"
+  },
+  {
+    "revision": "438ffb6c0deee256757bc5a37b0d05fe",
+    "url": "/Portfolio-REACT/static/media/TEMPLATE thumbnail.438ffb6c.png"
+  },
+  {
+    "revision": "c287fbee5325a0bd680d2e638116cdb1",
+    "url": "/Portfolio-REACT/static/media/WEATHER thumbnail.c287fbee.png"
+  },
+  {
+    "revision": "afdd124f5fcb607c9cdbacb95cd276a5",
+    "url": "/Portfolio-REACT/static/media/WORKDAY thumbnail.afdd124f.png"
+  },
+  {
+    "revision": "626b13215cc0427cbe17415bdd3ea7ea",
+    "url": "/Portfolio-REACT/static/media/candp.626b1321.jpg"
+  },
+  {
+    "revision": "47c9ef417673dd534a3e686a844dd433",
+    "url": "/Portfolio-REACT/static/media/nodejs.47c9ef41.png"
+  },
+  {
+    "revision": "17b035f1fb9b99efb7cb75195f829ac8",
+    "url": "/Portfolio-REACT/static/media/profile.17b035f1.jpg"
+  }
+]);
